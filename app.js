eb4c82ad47f2858c40d1c45716ef335d27bcdef8
Faraz Tariq <faraztariq12123@gmail.com>
2023-03-17 17:52:23 +0500

    matrix sum app

---
+++ app.js
@@ -0,0 +1,42 @@
+function sum(){
+var num1 =parseInt(document.getElementById('inputfield1').value);
+var num2 =parseInt(document.getElementById('inputfield10').value);
+var result1 = num1 + num2;
+var num3 =parseInt(document.getElementById('inputfield2').value);
+var num4 =parseInt(document.getElementById('inputfield11').value);
+var result2 = num3 + num4;
+var num5 =parseInt(document.getElementById('inputfield3').value);
+var num6 =parseInt(document.getElementById('inputfield12').value);
+var result3 = num5 + num6;
+var num7 =parseInt(document.getElementById('inputfield4').value);
+var num8 =parseInt(document.getElementById('inputfield13').value);
+var result4 = num7 + num8;
+var num9 =parseInt(document.getElementById('inputfield5').value);
+var num10 =parseInt(document.getElementById('inputfield14').value);
+var result5 = num9 + num10;
+var num11 =parseInt(document.getElementById('inputfield6').value);
+var num12 =parseInt(document.getElementById('inputfield15').value);
+var result6 = num11 + num12;
+var num13 =parseInt(document.getElementById('inputfield7').value);
+var num14 =parseInt(document.getElementById('inputfield16').value);
+var result7 = num13 + num14;
+var num15 =parseInt(document.getElementById('inputfield8').value);
+var num16 =parseInt(document.getElementById('inputfield17').value);
+var result8 = num15 + num16;
+var num17 =parseInt(document.getElementById('inputfield17').value);
+var num18 =parseInt(document.getElementById('inputfield18').value);
+var result9 = num17 + num18;
+
+document.getElementById('result1').value = result1;
+document.getElementById('result2').value = result2;
+document.getElementById('result3').value = result3;
+document.getElementById('result4').value = result4;
+document.getElementById('result5').value = result5;
+document.getElementById('result6').value = result6;
+document.getElementById('result7').value = result7;
+document.getElementById('result8').value = result8;
+document.getElementById('result9').value = result9;
+
+
+}
+sum();
